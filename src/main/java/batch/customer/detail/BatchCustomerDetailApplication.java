aeb4fd2f2dac21bd31c828c84729ad1ac6c34aa7
STEFAN FERDINAND <stefan.ferdinand75@gmail.com>
2024-06-02 13:47:33 +0700

    initial commit

---
+++ src/main/java/batch/customer/detail/BatchCustomerDetailApplication.java
@@ -0,0 +1,13 @@
+package batch.customer.detail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchCustomerDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchCustomerDetailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/batch/customer/detail/BatchCustomerDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package batch.customer.detail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchCustomerDetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
